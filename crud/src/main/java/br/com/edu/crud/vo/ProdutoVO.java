27f9364faa47b7852b6c608ecb4ed83805a957e2
eduardowmu <30440158+eduardowmu@users.noreply.github.com>
2021-10-10 22:58:34 -0300

    Microserviços com spring boot - Netflix

---
+++ crud/src/main/java/br/com/edu/crud/vo/ProdutoVO.java
@@ -0,0 +1,48 @@
+package br.com.edu.crud.vo;
+
+import java.io.Serializable;
+
+import org.modelmapper.ModelMapper;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import br.com.edu.crud.model.Produto;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+/*Essa notação que irá definir a órdem
+ *que os atributos irão retornar assim
+ *que uma requisição for realizada*/
+@JsonPropertyOrder(
+		{"id", 
+		 "nome",
+		 "estoque",
+		 "preco"})
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ProdutoVO implements Serializable
+{	private static final long serialVersionUID = 1L;
+	
+	@JsonProperty("id")
+	private Long id;
+	
+	@JsonProperty("nome")
+	private String nome;
+	
+	@JsonProperty("estoque")
+	private Integer estoque;
+	
+	@JsonProperty("preco")
+	private Double preco;
+	
+	public static ProdutoVO create(Produto produto)
+	{return new ModelMapper().map(produto, ProdutoVO.class);}
+}
